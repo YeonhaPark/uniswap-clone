62695347d8bf2743eee08bc2de8d28837a9adf60
YeonhaPark <yonaprisca@gmail.com>
2025-04-23 23:39:12 +0900

    feat: add switch-currency-button component

---
+++ components/icons/arrow-down.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+const SvgComponent = () => (
+  <svg
+    fill="none"
+    strokeWidth={2}
+    style={{
+      width: 24,
+      height: 24,
+      color: "#131313",
+      transform: "rotateZ(0deg)",
+    }}
+    viewBox="0 0 24 24"
+  >
+    <path
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M12 5v14M19 12l-7 7-7-7"
+    />
+  </svg>
+);
+export default SvgComponent;
---
+++ components/switch-currency-button.tsx
@@ -0,0 +1,15 @@
+import ArrowDown from "@/components/icons/arrow-down";
+
+export default function SwitchCurrencyButton() {
+  return (
+    <div className="relative z-10">
+      <div className="flex items-center">
+        <div className="absolute bottom-[-26px] left-1/2 -translate-x-1/2">
+          <div className="bg-surface2 hover:bg-surface2-hovered flex basis-auto cursor-pointer items-center rounded-2xl border-4 border-white p-2">
+            <ArrowDown />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
